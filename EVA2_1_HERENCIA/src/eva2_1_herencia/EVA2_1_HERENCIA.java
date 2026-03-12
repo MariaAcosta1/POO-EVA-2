021af6a1b02e2e5152a37cbb38f545f62113fa08
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2026-03-11 19:45:26 -0600

    Add files via upload

---
+++ EVA2_1_HERENCIA/src/eva2_1_herencia/EVA2_1_HERENCIA.java
@@ -0,0 +1,15 @@
+
+package eva2_1_herencia;
+
+public class EVA2_1_HERENCIA {
+
+    public static void main(String[] args) {
+        Ejemplo ejemplo = new Ejemplo();
+        System.out.println(ejemplo.toString());
+    }
+    
+}
+
+class Ejemplo{
+    
+}
