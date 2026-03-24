adff4454c77600311e84276eb4c662e026163016
MariaAcosta1 <L25550705@chihuahua2.tecnm.mx>
2026-03-23 19:56:46 -0600

    Add files via upload

---
+++ EVA2_7_ABSTRACT/src/eva2_7_abstract/Circulo.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva2_7_abstract;
+
+public class Circulo extends Figuras{
+    private double radio;
+    
+    public Circulo(){
+        this.radio = 0;  
+    }
+    public Circulo(double radio){
+        this.radio = radio;
+    }
+    public double getRadio() {
+        return radio;
+    }
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    @Override
+    public double calcularArea(){
+        return Math.PI * (radio);
+    }
+    @Override
+    public double calcularPerimetro(){
+        return (2 * radio) * Math.PI;
+    }
+    @Override
+    public String toString(){
+        String resu = "";
+        resu = "----Circulo----" + "\n" + 
+                "Area de un circulo: " + calcularArea() + "\n" +
+                "Perimetro de un circulo: " + calcularPerimetro() + "\n";
+        return resu;
+    }
+}
---
+++ EVA2_7_ABSTRACT/src/eva2_7_abstract/EVA2_7_ABSTRACT.java
@@ -0,0 +1,13 @@
+
+package eva2_7_abstract;
+
+public class EVA2_7_ABSTRACT {
+
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(4.0);
+        System.out.println(circulo);
+        Triangulo triangulo = new Triangulo(2.0, 4.0, 9.0);
+        System.out.println(triangulo);
+    }
+    
+}
